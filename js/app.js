52192ade1039fccb26926840b7899620eaa94b36
Daria <daria@Daria-Rejdych-MBP.local>
2016-07-29 12:35:51 +0200

    our-team lider

--- js/app.js
+++ js/app.js
@@ -1,33 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //ex.1
-    var nextPicture = document.querySelector(".nextPicture");
-    var prevPicture = document.querySelector(".prevPicture");
-    //pobieramy wszystkie li
-    var allImg = document.querySelector("div.slider").childern;
-    //indeks biezącego widoku obrazka
-    var currentImg = 0;
-    //dodajemy do 1-go obrazka (pojedynczy element)
-    //klasę visible
-    allImg[currentImg].classList.add("visible");
+$(document).on('ready', function() {
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: '.next_picture',
+        prevArrow: '.prev_picture'
+    });
 
-    nextPicture.addEventListener("click", function() {
-        console.log("kliknięto NEXT");
-        allImg[currentImg].classList.remove("visible");
-        currentImg++
-        if (currentImg >= allImg.length) {
-
-            currentImg=0;
-        }
-        allImg[currentImg].classList.add("visible");
-    })
-    prevPicture.addEventListener("click", function() {
-        console.log("kliknięto PREV");
-        allImg[currentImg].classList.remove("visible");
-        currentImg--;
-        if (currentImg < 0) {
-            // currentImg = 0;
-            currentImg = allImg.length - 1
-        }
-        allImg[currentImg].classList.add("visible")
-    })
 });
