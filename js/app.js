9057e24700acf35e69bb8cc62748121a7b7a9d53
Daria <daria@Daria-Rejdych-MBP.local>
2016-08-03 16:14:23 +0200

    progressbar done

--- js/app.js
+++ js/app.js
@@ -8,4 +8,26 @@ $(document).on('ready', function() {
         prevArrow: '.prev_picture'
     });
 
+    $(function() {
+        $(".progressbar-1").progressbar({
+            value: 90
+        });
+    });
+    $(function() {
+        $(".progressbar-2").progressbar({
+            value: 70
+        });
+    });
+    $(function() {
+        $(".progressbar-3").progressbar({
+            value: 75
+        });
+    });
+    $(function() {
+        $(".progressbar-4").progressbar({
+            value: 80
+        });
+    });
+
+
 });
